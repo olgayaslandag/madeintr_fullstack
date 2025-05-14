4e48b622f537d2f04b192980db954090c1fe39d8
Olgay <olgayaslandag@gmail.com>
2025-05-15 00:36:11 +0300

    remove ai modal repo interface

--- app/Contracts/Ai/AiInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Contracts\Ai;
-
-interface AiInterface
-{
-    public function find(array $where = []): ?\Illuminate\Database\Eloquent\Model;
-    public function store(array $data, int $id = null);
-}
